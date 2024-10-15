cd3a08d5568a9039159486937472488ad220bf39
lai2-24 <lai2-24@itismeucci.com>
2024-10-15 09:18:57 +0200

    ok

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -6,22 +6,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
-        System.out.println("Client partito!");
+        System.out.println("client avviato");
         Socket s = new Socket("localhost", 3000);
-        System.out.println("Client collegato");
+        System.out.println("client connesso");
 
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        BufferedReader inTastiera = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Parola: ");
-        String messagio = inTastiera.readLine();
-        out.writeBytes(messagio + '\n');
+        do{
+            System.out.println("inserisci la parola: ");
+            Scanner scan = new Scanner(System.in);
+            String stringaDigitata = scan.nextLine();
 
-        String serverMessage = in.readLine();
-        System.out.println("Server message: " + serverMessage);
+            if(stringaDigitata.equals("exit")){
+                System.out.println("il client vuole chiudere");
+                out.writeBytes("!" + "\n");
+                break;
+            }
+
+            out.writeBytes(stringaDigitata + "\n");
+            String stringaRicevuta = in.readLine();
+            System.out.println("il server risponde con " + stringaRicevuta);
+
+        }while(true);
     }
 }
